7cc57feda0aef6d872bdee2fbd43df8b7c718aa3
Johan Özbek <JohanOzbek@gmail.com>
2020-08-19 12:13:46 +0200

    committing beginning branch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hejsan");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+
+            System.out.println("Skriv in första talet");
+            int first = scanner.nextInt();
+
+            System.out.println("Skriv in andra talet");
+            int second = scanner.nextInt();
+
+            System.out.println("Välj räknesätt *, /, +, -");
+            String arithmetic = scanner.nextLine();
+            if(arithmetic == "*"){
+                System.out.println(first * second);
+            }
+        }
+
     }
 }
